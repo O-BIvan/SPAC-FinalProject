94989650048e567b82fde36282de6aee160022de
O-BIvan <nigerianprince@no.scam>
2020-09-18 00:27:18 +0300

    Add locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,15 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    ADDED_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, ".alert-success:first-child strong")
+    SUCCESS_ADD_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child")
+    BASKET_ADD_MESSAGE = (By.CSS_SELECTOR, ".alert-info")
+
+
