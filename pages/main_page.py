fa48b8145f91899b2920cd092a6adafdbe1035b6
O-BIvan <nigerianprince@no.scam>
2020-09-18 00:27:43 +0300

    Fix typo

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
 
@@ -10,4 +8,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not present'
