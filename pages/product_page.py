8ca5a9e102301605a26eab4b1219e6251061c11b
O-BIvan <nigerianprince@no.scam>
2020-09-19 00:35:17 +0300

    Modify methods to improve readability

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,36 +3,24 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    product_name = ''
-    product_price = ''
-
     def add_product_to_cart(self):
-        self.product_name_is()
-        self.product_price_is()
-
         add_to_basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         add_to_basket.click()
-        self.solve_quiz_and_get_code()
-        self.add_product_success_message()
-        self.add_product_price_match()
-        self.add_product_names_match()
-
-    def product_name_is(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product Name is not found"
-        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-
-    def product_price_is(self):
-        assert  self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product Price is not found"
-        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-
-    def add_product_success_message(self):
-        assert self.is_element_present(*ProductPageLocators.SUCCESS_ADD_MESSAGE), \
-            "No message about successful addition "
 
     def add_product_names_match(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
-        assert self.product_name == added_product_name, "Product and Added Product names don't match"
+        assert product_name == added_product_name, "Product and Added Product names don't match"
 
     def add_product_price_match(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         added_product_price = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_PRICE).text
-        assert self.product_price == added_product_price, "Product and Added Product prices don't match"
+        assert product_price == added_product_price, "Product and Added Product prices don't match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_ADD_MESSAGE), \
+            "Success message should disappear, but it is not"
