ff2985c8d1216f342b02a5475ec153b1d5fc3045
O-BIvan <nigerianprince@no.scam>
2020-09-19 00:36:07 +0300

    Add success_message tests

--- test_product_page.py
+++ test_product_page.py
@@ -5,11 +5,37 @@ import pytest
 # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
-@pytest.mark.parametrize('number', [0, 1, 2, 3, 4, 5, 6,
-                                    pytest.param(7, marks=pytest.mark.xfail),
-                                    8, 9])
-def test_guest_can_add_product_to_basket(browser, number):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
+    page.solve_quiz_and_get_code()
+    page.add_product_names_match()
+    page.add_product_price_match()
+
+
+@pytest.mark.skip(reason="Not in the test scope")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip(reason="Not in the test scope")
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip(reason="Not in the test scope")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.success_message_should_disappear()
