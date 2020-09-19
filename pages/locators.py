5dfb89d22351b1263a5e78286320b4f0a1c6fac4
O-BIvan <nigerianprince@no.scam>
2020-09-19 20:59:28 +0300

    Move locators from MainPageLocators to BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -18,5 +14,11 @@ class ProductPageLocators:
     ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, ".alert-success:first-child strong")
     SUCCESS_ADD_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child")
     BASKET_ADD_MESSAGE = (By.CSS_SELECTOR, ".alert-info")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 
 
