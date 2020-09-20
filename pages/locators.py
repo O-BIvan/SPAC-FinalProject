7eaee0cbec379995a9ccabf54bf9bc5d4ad05acb
O-BIvan <nigerianprince@no.scam>
2020-09-20 21:20:03 +0300

    Remove redundant locators

--- pages/locators.py
+++ pages/locators.py
@@ -14,8 +14,6 @@ class BasketPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
-    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form button.btn-primary")
 
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
