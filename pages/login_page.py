af9a737592da297ba2a5de7281be4513538b4678
O-BIvan <nigerianprince@no.scam>
2020-09-20 20:49:22 +0300

    Add register_user method

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,3 +16,16 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Page doesn't contain: register form"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REG_EMAIL)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.REG_PASSWORD)
+        password_field.send_keys(password)
+        password_confirm = self.browser.find_element(*LoginPageLocators.REG_PASSWORD_CONFIRM)
+        password_confirm.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        reg_button.click()
+
+
+
